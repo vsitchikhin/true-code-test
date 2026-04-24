70a182be70914160fdfad544a11c5cafd5a5fe64
Vladimir Sitchikhin <vsitchikhin@gmail.com>
2026-04-24 19:18:43 +0300

    fix: правка е2е тестов

--- front/playwright.config.ts
+++ front/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: 1,
   reporter: 'html',
   use: {
-    baseURL: 'http://127.0.0.1:5173',
+    baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
   },
